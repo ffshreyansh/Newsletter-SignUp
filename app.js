fe19eef45b596c0933de0eb30c49721852de9628
Shreyansh Kumar <shreyanshkumar058@gmail.com>
2022-12-19 17:15:12 +0530

    success and failure pages added

--- app.js
+++ app.js
@@ -34,6 +34,13 @@ app.post("/", function(req, res){
         auth: "shreyanshKr:f29b84a229e8f6325b3c75fa241fdff3-us21"
     }
    const request =  https.request(url, options, function(response){
+
+        if(response.statusCode === 200){
+            res.sendFile(__dirname + "/success.html");
+        }else{
+            res.sendFile(__dirname + "/failure.html")
+        }
+
         response.on("data", function(data){
             console.log(JSON.parse(data));
         })
